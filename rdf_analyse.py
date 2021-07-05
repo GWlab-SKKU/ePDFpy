34d0cf3f248129b36362de30166cb137510743a0
pilsung <pilsungdev@gmail.com>
2021-07-05 18:50:57 +0900

    rdf-analyse

--- rdf_analyse.py
+++ rdf_analyse.py
@@ -1,12 +1,34 @@
 import typing
+from datacube import DataCube
+import pyqtgraph as pg
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 class rdf_analyse(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, datacube: DataCube):
         QtWidgets.QWidget.__init__(self)
         print("init")
-        self.setGeometry(300, 300, 300, 200)
-        self.layout = QtWidgets.QHBoxLayout()
+        self.datacube = datacube
+        self.initui()
+
+
+    def initui(self):
+        self.setMinimumSize(500, 500)
+        self.layout = QtWidgets.QGridLayout()
         self.layout.addWidget(QtWidgets.QPushButton("hello"))
+        self.controlPanel = controlPanel()
+        self.graph_Iq = pg.plot()
+        self.graph_phiq = pg.plot()
+        self.graph_
+        self.layout.addWidget(self.controlPanel,0,0)
+        self.layout.addWidget(self.controlPanel,1,0)
         self.setLayout(self.layout)
         self.show()
+
+    def update_graph(self):
+        pass
+
+class controlPanel(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+
+
