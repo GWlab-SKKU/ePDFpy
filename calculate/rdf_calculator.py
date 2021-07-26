c7e880aa145f11cba71ae137889b0259e425857d
pilsung <pilsungdev@gmail.com>
2021-07-26 20:17:29 +0900

    for r10 to r30 script

--- calculate/rdf_calculator.py
+++ calculate/rdf_calculator.py
@@ -7,6 +7,9 @@ paramK = np.loadtxt("./assets/Parameter_files/Kirkland_2010.txt")
 
 def calculation(ds, q_start_num, q_end_num, element_nums, ratio, azavg, is_full_q, damping, rmax, dr, fit_at_q=None, N=None):
     element_nums = np.array(element_nums)
+    for idx, element in enumerate(element_nums):
+        if element == 0:
+            ratio[idx] = 0
     element_nums = element_nums[element_nums != 0]
     ratio = np.array(ratio)
     ratio = ratio[ratio != 0]
@@ -37,7 +40,7 @@ def calculation(ds, q_start_num, q_end_num, element_nums, ratio, azavg, is_full_
 
     # added code
     if fit_at_q is not None:
-        search_q = q[q < fit_at_q]
+        search_q = q[q <= fit_at_q + 0.01]  # todo
     else :
         search_q = q
     fit_at_q, qpos = search_q.max(), search_q.argmax()  # qmax = q_fix
@@ -71,6 +74,74 @@ def calculation(ds, q_start_num, q_end_num, element_nums, ratio, azavg, is_full_
     Gr = 8 * np.pi * phiq_damp @ np.sin(q[:, None] * r) * ds
 
     return q, r, Iq, Autofit, phiq, phiq_damp, Gr, SS, fit_at_q, N
+
+
+def _calculation_with_q(ds, q, Iq, element_nums, ratio, is_full_q, damping, rmax, dr, fit_at_q=None, N=None):
+    element_nums = np.array(element_nums)
+    for idx, element in enumerate(element_nums):
+        if element == 0:
+            ratio[idx] = 0
+    element_nums = element_nums[element_nums != 0]
+    ratio = np.array(ratio)
+    ratio = ratio[ratio != 0]
+    e_tot = np.sum(np.array(ratio))
+    e_ratio = ratio / e_tot
+
+    s = q / 2 / np.pi
+    s2 = s ** 2
+    L = np.uint16(len(q))
+    paramK_elems = paramK[element_nums, :]
+    f = np.array([KirklandFactors(s2, paramK_elem) for paramK_elem in paramK_elems])
+    fq = np.sum(f * e_ratio[:, None], axis=0)  # fq.shape = 2366,
+    fq_sq = fq ** 2
+    gq = np.sum(f ** 2 * e_ratio[:, None], axis=0)
+
+    if is_full_q:
+        AFrange = 0
+    else:
+        AFrange = int(2 / 3 * L)
+
+    wi = np.ones((L, 1))
+    wi[0:AFrange] = 0
+
+    # added code
+    if fit_at_q is not None:
+        search_q = q[q <= fit_at_q + 0.01]  # todo
+    else:
+        search_q = q
+    fit_at_q, qpos = search_q.max(), search_q.argmax()  # qmax = q_fix
+    # end
+
+    # qmax, qpos = q.max(), q.argmax()  # qmax = q_fix
+    fqfit = gq[qpos]
+    iqfit = Iq[qpos]
+
+    if N is None:
+        a1 = np.sum(wi * gq * Iq)
+        a2 = np.sum(wi * Iq * fqfit)
+        a3 = np.sum(wi * gq * iqfit)
+        a4 = np.sum(wi) * fqfit * iqfit
+        a5 = np.sum(wi * gq ** 2)
+        a6 = 2 * np.sum(wi * gq * fqfit)
+        a7 = np.sum(wi) * fqfit * fqfit
+        N = (a1 - a2 - a3 + a4) / (a5 - a6 + a7)
+
+    C = iqfit - N * fqfit
+
+    Autofit = N * gq + C
+
+    SS = np.sum((Iq - Autofit) ** 2);
+
+    r = np.arange(0.01, rmax + dr, dr)  # rmax+dr to fit the eRDF
+
+    phiq = ((Iq - Autofit) * s) / (N * fq_sq);
+    phiq_damp = phiq * np.exp(-s2 * damping)
+
+    Gr = 8 * np.pi * phiq_damp @ np.sin(q[:, None] * r) * ds
+
+    return q, r, Iq, Autofit, phiq, phiq_damp, Gr, SS, fit_at_q, N
+
+
 
 
 def KirklandFactors(s2, paramK_element):
