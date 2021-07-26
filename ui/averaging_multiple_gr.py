09de2b071bf2a5cf978f478e44df0b10c45f1a03
pilsungdev <pilsungdev@gmail.com>
2021-07-27 03:47:00 +0900

    gui update

--- ui/averaging_multiple_gr.py
+++ ui/averaging_multiple_gr.py
@@ -3,7 +3,10 @@ import ui.main as main
 import pyqtgraph as pg
 import os
 import glob
+import pandas as pd
 from pathlib import Path
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import Qt
 
 class Viewer(QtWidgets.QWidget):
 
@@ -15,6 +18,7 @@ class Viewer(QtWidgets.QWidget):
         super().__init__()
         self.initui()
         self.grCubes = []
+        self.binding()
 
     def initui(self):
         self.layout = QtWidgets.QVBoxLayout()
@@ -25,34 +29,52 @@ class Viewer(QtWidgets.QWidget):
         self.layout.addWidget(self.rightPanel)
 
     def binding(self):
-        self.leftPanel.btn_open_folder.clicked.connect()
+        self.leftPanel.btn_open_folder.clicked.connect(self.open_btn_clicked)
 
     def open_btn_clicked(self):
         path = QtWidgets.QFileDialog.getExistingDirectory(self,'open')
+        self.setWindowTitle(path)
         if path == "":
             return
-        units = self.get_csv_files_from_folder(path)
-        for unit in units:
+        data_r_files = self.get_csv_files_from_folder(path)
+        if len(data_r_files) == 0 :
+            QMessageBox.about(self,"Error","No data detected on the folder")
+        ################## MAIN ####################
+        for idx, data_r_file in enumerate(data_r_files):
             grCube = GrCube()
-            grCube.unit_path = unit
-            grCube.unit_path
-            self.grCubes.append()
+            grCube.unit_path = data_r_file
 
+            # graph name
+            graph_name = data_r_file[data_r_file.rfind("_")+1:data_r_file.find("Data_r.csv")]
+
+            # load and plot
+            r, Gr = self.load_Gr_file(data_r_file)
+            plot_ = self.rightPanel.graphView.plot(r,Gr,name=graph_name,pen=pg.mkPen(pg.intColor(idx)))
+            grCube.plotItem = plot_
+            grCube.chkbox_module = self.leftPanel.graphGroup.add_module(os.path.split(data_r_file)[1],"yellow")
+            print(grCube.chkbox_module.chkbox)
+            grCube.chkbox_module.chkbox.clicked.connect(grCube.plot_show_hide)
+            self.grCubes.append(grCube)
+
+
+
+
+    def load_Gr_file(self, path):
+        df = pd.read_csv(path)
+        r = df['r'].to_numpy()
+        Gr = df['Gr'].to_numpy()
+        return r, Gr
 
 
     def get_csv_files_from_folder(self, folder):
-        csvfiles = Path("/mnt/experiment/TEM diffraction/").rglob("Data_r.csv")
-        fps = set()
+        csvfiles = Path(folder).rglob("*Data_r.csv")
+        fps = []
         for file in csvfiles:
             if file.name in ["diagonal.csv", "diagonal_1.csv", "line.csv"]:
                 continue
             if "r30" in file.name:
                 continue
-            fp = str(file.absolute())
-            idx = fp.find('\\')
-            fps.add(fp[:idx])
-        fps = list(fps)
-        fps.sort()
+            fps.append(str(file.absolute()))
         return fps
 
 
@@ -62,13 +84,21 @@ class GrCube:
         self.data_r_path = None
         self.data_q_path = None
 
-        self.folder_path = None
+        self.selected_folder = None
         self.unit_path = None
         self.unit_name = None
         self.Gr_path = None
         self.plotItem = None
-        self.chkbox = None
+        self.chkbox_module = None
         self.Gr = None
+
+    def plot_show_hide(self):
+        if self.chkbox_module.chkbox.isChecked():
+            print("show, ", self)
+            self.plotItem.show()
+        else:
+            print("hide",self)
+            self.plotItem.hide()
 
 
 class GraphPanel(QtWidgets.QWidget):
@@ -94,29 +124,43 @@ class LeftPanel(QtWidgets.QWidget):
 class GraphGroup(QtWidgets.QGroupBox):
     def __init__(self):
         super().__init__()
-        layout = QtWidgets.QVBoxLayout()
-        self.setLayout(layout)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.layout)
 
-        box1 = GraphModule()
-        box1.set_text("Hello")
-        box1.set_color("Yellow")
-        layout.addWidget(box1)
+    def add_module(self, text, color):
+        module = GraphModule()
+        module.set_text(text)
+        module.set_color(color)
+        self.layout.addWidget(module)
+        return module
+
 
 
 class GraphModule(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         layout = QtWidgets.QHBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+        layout.setSpacing(0)
         self.setLayout(layout)
         self.chkbox = QtWidgets.QCheckBox("")
+        self.chkbox.setChecked(True)
+        self.textbox = QtWidgets.QPlainTextEdit("")
+        self.textbox.setReadOnly(True)
+        self.textbox.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+
+        self.textbox.setMaximumHeight(20)
         layout.addWidget(self.chkbox)
+        layout.addWidget(self.textbox)
 
     def set_text(self,text):
-        self.chkbox.setText(text)
+        self.textbox.setPlainText(text)
         pass
 
     def set_color(self,color):
-        self.chkbox.setStyleSheet("background-color: {}".format(color))
+        self.chkbox.setStyleSheet("background-color: {}".format("yellow"))
+        # self.chkbox.styleSheet()
         pass
 
 
