b0d5837087cf0385148f2a8b799acb7b09bbb2a2
pilsung <pilsungdev@gmail.com>
2021-08-14 20:12:27 +0900

    feature - add crosshair
    In graph, you can add cross hair by ctrl + mouse left click
    And you can move crosshair with left and right arrow keys

--- ui/ui_util.py
+++ ui/ui_util.py
@@ -83,6 +83,7 @@ class CoordinatesPlotWidget(pg.PlotWidget):
         # self.coor_label.setParentItem(self.getViewBox())
         # self.coor_label.setPos(10,10)
 
+        # self.cross_hair = self.plot()
 
         # legend = self.addLegend()
         if offset is None:
@@ -109,9 +110,88 @@ class CoordinatesPlotWidget(pg.PlotWidget):
         return super(CoordinatesPlotWidget, self).mouseMoveEvent(ev)
 
     def mousePressEvent(self, ev):
+        qp = self.plotItem.vb.mapSceneToView(ev.localPos())
+        modifiers = QtGui.QApplication.keyboardModifiers()
+
+        if modifiers == QtCore.Qt.ShiftModifier:
+            print('Shift+Click')
+            self.coor_update_toggle = not self.coor_update_toggle
+        elif modifiers == QtCore.Qt.ControlModifier:
+            if hasattr(self, 'crosshair_plot') and self.crosshair_plot is not None:
+                self.removeItem(self.crosshair_plot)
+                self.crosshair_plot = None
+            else:
+                self.crosshair_curve_dataItem, self.crosshair_idx = self.find_closest_coor(qp.x(), qp.y())
+                self.create_cross_hair()
+            print('Control+Click')
+        elif modifiers == (QtCore.Qt.ControlModifier |
+                           QtCore.Qt.ShiftModifier):
+            print('Control+Shift+Click')
+        else:
+            print('Click')
+        self.find_closest_coor(qp.x(),qp.y())
+
         # print(self.getPlotItem().dataItems[0].xDisp) # xData, yData, xDisp, yDisp
-        self.coor_update_toggle = not self.coor_update_toggle
+
         return super().mousePressEvent(ev)
+
+    def keyPressEvent(self, ev):
+        if ev.key() == QtCore.Qt.Key.Key_Right:
+            self.move_cross_hair(+1)
+        elif ev.key() == QtCore.Qt.Key.Key_Left:
+            self.move_cross_hair(-1)
+        super().keyPressEvent(ev)
+
+    def create_cross_hair(self):
+        self.crosshair_plot = self.plot(symbol='+', symbolSize=20, pen='b',symbolPen=pg.mkPen(width=1),symbolBrush=('b'),name="cross")
+        legend = self.addLegend()
+        self.crosshair_legend_label = legend.items[-1][-1]
+        self.set_cross_hair_coordi()
+
+    def move_cross_hair(self, x_increase):
+        self.crosshair_idx = self.crosshair_idx+x_increase
+        self.set_cross_hair_coordi()
+
+    def set_cross_hair_coordi(self):
+        x = self.crosshair_curve_dataItem.xData[self.crosshair_idx]
+        y = self.crosshair_curve_dataItem.yData[self.crosshair_idx]
+        self.crosshair_plot.setData([x],[y])
+        str_x = str(np.round(x,2))
+        str_y = str(np.round(y,2))
+        self.crosshair_legend_label.setText("x:{},y:{}".format(str_x, str_y))
+
+    def find_closest_coor(self, x,y):
+        """
+        :param x:
+        :param y:
+        :return: dataItem, idx
+        """
+        if len(self.getPlotItem().dataItems) == 0:
+            return
+        dataitem_idx_distance = []
+        for dataItem in self.getPlotItem().dataItems:
+            if dataItem.xData is None:
+                continue
+            x_data = np.asarray(dataItem.xData)
+            y_data = np.asarray(dataItem.yData)
+            distance_arr = np.abs(x_data-x)
+            idx = distance_arr.argmin()
+            distance = (x_data[idx]-x)**2 + (y_data[idx]-y)**2
+            dataitem_idx_distance.append([dataItem,idx,distance])
+
+        print("whole dataitem idx distance",dataitem_idx_distance)
+        cloest_dataItem = dataitem_idx_distance[0]
+        for dataItem, idx, distance in dataitem_idx_distance[1:]:
+            if distance < cloest_dataItem[2]:
+                cloest_dataItem = [dataItem, idx, distance]
+        return cloest_dataItem[0],cloest_dataItem[1]
+
+
+
+    def find_nearest(self, arr, value):
+        arr = np.asarray(arr)
+        idx = (np.abs(arr - value)).argmin()
+        return arr[idx] , idx
 
     def setYScaling(self, bool):
         if bool:
@@ -122,6 +202,10 @@ class CoordinatesPlotWidget(pg.PlotWidget):
     def YScaling(self):
         self.enableAutoRange(axis='y')
         self.setAutoVisible(y=True)
+
+
+
+
 
 def update_value(widget:QtWidgets.QWidget, value):
     """ update value without occuring signal """
