ba56e57532ee9b99daf9a688713302887d990d8f
pilsungdev <pilsungdev@gmail.com>
2022-03-11 17:53:39 +0900

    feature - mask module (1)

--- ui/roi_selector/mask_module.py
+++ ui/roi_selector/mask_module.py
@@ -19,7 +19,7 @@ class MaskModule():
         self.imageView = imageView
         self.mask_dict = {}
 
-        self.roi_creator = RoiCreater(self)
+        self.roi_creator = None
         self.dropdown = DropDown(self)
         self.list_widget = ListWidget(self)
 
@@ -29,11 +29,12 @@ class MaskModule():
         self.img = img
         self.dropdown.img = img
         self.list_widget.img = img
-        self.roi_creator.img = img
 
     def get_current_mask(self):
         x_y = self.mask_dict[self.dropdown.currentText()]['data']
-        return
+        img = np.zeros(self.img.shape)
+        cv2.fillPoly(img, pts=[x_y], color=(255, 255, 255))
+        return img
 
     def _load_mask(self):
          self.image = json.load(definitions.MASK_PATH)
@@ -41,13 +42,14 @@ class MaskModule():
     def _mask_reload(self):
         self.mask_dict
         self.dropdown.mask_reload()
-        # self.list_widget.mask_reload()
-
+        self.list_widget.mask_reload()
 
 class RoiCreater(QtWidgets.QWidget):
-    def __init__(self, module:MaskModule):
+    def __init__(self, module:MaskModule, img, name=None, pnts=None):
         QtWidgets.QWidget.__init__(self)
         self.module = module
+        self.img = img
+        self.pnts = pnts
         # self.setWindowFlags(self.windowFlags() | Qt.Qt.Window)
 
         layout = QtWidgets.QVBoxLayout()
@@ -87,8 +89,14 @@ class RoiCreater(QtWidgets.QWidget):
         # self.setBaseSize(800,800)
         self.setMinimumSize(800,700)
 
-        self.update_image()
-        self.draw_roi()
+        self.update_image(self.img)
+        if pnts is None:
+            self.draw_roi()
+        else:
+            self.draw_roi(pnts)
+
+        if name is not None:
+            self.txt_name.setText(name)
 
         self.binding()
 
@@ -106,7 +114,7 @@ class RoiCreater(QtWidgets.QWidget):
     def update_image(self, img=None):
         if img is None:
             img = self.module.img
-        if not img:
+        if img is None:
             return
         if self.radio_raw.isChecked():
             self.imageView.setImage(img)
@@ -116,15 +124,15 @@ class RoiCreater(QtWidgets.QWidget):
             self.imageView.setImage(np.log((np.abs(img)+1)))
 
     def draw_roi(self, pnts=None):
-        if not self.module.img:
+        if self.module.img is None:
             return
-        if not pnts:
+        if pnts is None:
             w = self.module.img.shape[0]
             h = self.module.img.shape[1]
             pnts = [[int(w / 2) - int(w / 10), int(h / 2) - int(h / 10)],
-                    [int(w / 2) - int(w / 10), int(h / 2) + int(h / 10)],
                     [int(w / 2) + int(w / 10), int(h / 2) - int(h / 10)],
-                    [int(w / 2) + int(w / 10), int(h / 2) + int(h / 10)]]
+                    [int(w / 2) + int(w / 10), int(h / 2) + int(h / 10)],
+                    [int(w / 2) - int(w / 10), int(h / 2) + int(h / 10)]]
         self.poly_line_roi = pg.PolyLineROI(pnts, closed=True)
         self.imageView.addItem(self.poly_line_roi)
 
@@ -135,30 +143,35 @@ class RoiCreater(QtWidgets.QWidget):
         self.radio_root.toggled.connect(lambda x: self.update_image())
         self.radio_log.toggled.connect(lambda x: self.update_image())
 
-    def btn_ok_clicked(self):
+    def btn_export_clicked(self):
         handles = [handle.pos() for handle in self.poly_line_roi.getHandles()]
-        handles = np.array(handles)
+        handles = np.array(handles).astype(np.int)
+
         img = np.zeros(self.imageView.image.shape)
-        cv2.fillPoly(img, pts=[handles.astype(np.int)], color=(255, 255, 255))
+        cv2.fillPoly(img, pts=[handles], color=(255, 255, 255))
 
-        if self.save_mask_folder is None:
-            fp, _ = QFileDialog.getSaveFileName()
-            if fp == "":
-                return
-        else:
-            name = self.txt_name.toPlainText()
-            fp = os.path.join(self.save_mask_folder,name)
+        fp, _ = QFileDialog.getSaveFileName()
+        if fp == "":
+            return
 
-        Path(fp).mkdir(parents=True, exist_ok=True)
-
+        name = self.txt_name.toPlainText()
         if os.path.splitext(fp)[1] is None or os.path.splitext(fp)[1] != ".csv":
             fp = fp + ".csv"
-        np.savetxt(fp, img, delimiter=',', fmt='%s')
+        np.savetxt(name, img, delimiter=',', fmt='%s')
         print("save to {}".format(fp))
-        if self.func_after_mask_selected is not None:
-            self.func_after_mask_selected()
-        self.close()
         return
+
+    def btn_ok_clicked(self):
+        handles = np.array([handle.pos() for handle in self.poly_line_roi.getHandles()])
+        handles = handles + np.array(self.poly_line_roi.pos())
+        handles = handles.astype(int)
+        self.module.mask_dict.update(
+            {self.txt_name.toPlainText():
+                 {'size':self.module.img.shape,
+                  'data':handles}
+             })
+        self.module._mask_reload()
+        self.close()
 
     def btn_cancel_clicked(self):
         self.close()
@@ -188,14 +201,20 @@ class DropDown(QtWidgets.QComboBox):
         if idx == len(self.module.mask_dict) + 1:
             # edit
             self.module.list_widget.show()
+        if self.currentText() not in ['[Edit]','None', ''] and self.img is not None:
+            size = self.module.mask_dict[self.currentText()]['size']
+            shape = self.img.shape
+            if size != shape:
+                QtWidgets.QMessageBox.about(self,"",
+                        f"Size is not matching. \n mask size: {size}\n image size: {shape}")
+
 
 class ListWidget(QtWidgets.QWidget):
     def __init__(self, module:MaskModule):
         super().__init__()
         self.module = module
         self.QList = QtWidgets.QListWidget()
-        self.items = list(self.module.mask_dict.keys())
-        self.QList.addItems(self.items)
+        self.mask_reload()
         # self.QList.itemDoubleClicked.connect()
         self.layout = QtWidgets.QGridLayout()
         self.setLayout(self.layout)
@@ -219,52 +238,71 @@ class ListWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.btn_edit, 3, 2)
         self.layout.addWidget(self.btn_new, 4, 2)
 
+    def mask_reload(self):
+        self.QList.clear()
+        self.items = list(self.module.mask_dict.keys())
+        self.QList.addItems(self.items)
+
     def btn_move_up_clicked(self):
         indexes = self.QList.selectedIndexes()
-        if len(indexes) > 0:
-            try:
-                indexes.sort()
-                first_row = indexes[0].row() - 1
-                if first_row >= 0:
-                    for idx in indexes:
-                        if idx != None:
-                            row = idx.row()
-                            self.items.insert(row - 1, self.items[row])
-                            self.items.pop(row + 1)
-                            self.QList.clear()
-                            self.QList.addItems(self.items)
-            except Exception as e:
-                print(e)
-        else:
+        if len(indexes) <= 0:
             print('Select at least one item from list!')
+            return
+        try:
+            indexes.sort()
+            first_row = indexes[0].row() - 1
+            if first_row >= 0:
+                for idx in indexes:
+                    if idx != None:
+                        row = idx.row()
+                        self.items.insert(row - 1, self.items[row])
+                        self.items.pop(row + 1)
+                        self.QList.clear()
+                        self.QList.addItems(self.items)
+        except Exception as e:
+            print(e)
+        self.update_list_to_dropdown()
 
     def btn_move_down_clicked(self):
         max_row = len(self.items)
         indexes = self.QList.selectedIndexes()
-        if len(indexes) > 0:
-            try:
-                indexes.sort()
-                last_row = indexes[-1].row() + 1
-                if last_row < max_row:
-                    for idx in indexes:
-                        if idx != None:
-                            row = idx.row()
-                            self.items.insert(row + 2, self.items[row])
-                            self.items.pop(row)
-                            self.QList.clear()
-                            self.QList.addItems(self.items)
-            except Exception as e:
-                print(e)
-        else:
+        if len(indexes) <= 0:
             print('Select at least one item from list!')
-        pass
+            return
+        try:
+            indexes.sort()
+            last_row = indexes[-1].row() + 1
+            if last_row < max_row:
+                for idx in indexes:
+                    if idx != None:
+                        row = idx.row()
+                        self.items.insert(row + 2, self.items[row])
+                        self.items.pop(row)
+                        self.QList.clear()
+                        self.QList.addItems(self.items)
+        except Exception as e:
+            print(e)
+        self.update_list_to_dropdown()
 
+    def update_list_to_dropdown(self):
+        self.QList.update()
+        new_mask_dict = {}
+        if self.module is not None:
+            for i in range(len(self.QList)):
+                name = self.QList.item(i).text()
+                new_mask_dict.update({name:self.module.mask_dict[name]})
+            self.module.mask_dict = new_mask_dict
+            self.module._mask_reload()
 
     def btn_new_clicked(self):
-        self.module.roi_creator.start(new=True)
+        self.module.roi_creator = RoiCreater(self.module, self.module.img)
+        self.module.roi_creator.show()
 
     def btn_edit_clicked(self):
-        self.module.roi_creator.start(False)
+        self.module.roi_creator = RoiCreater(self.module, self.module.img,
+                                             self.QList.selectedItems()[0].text(),
+                                             self.module.mask_dict[self.QList.selectedItems()[0].text()]['data'])
+        self.module.roi_creator.show()
 
     def btn_del_clicked(self):
         indexes = self.QList.selectedIndexes()
@@ -280,6 +318,3 @@ class ListWidget(QtWidgets.QWidget):
                 self.items.pop(idx)
             self.QList.clear()
             self.QList.addItems(self.items)
-
-
-
